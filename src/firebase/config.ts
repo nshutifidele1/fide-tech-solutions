cb2b41c9389e99d3bcf4b71f37e92e145c69bbbf
Fidele <nshutifidele1@gmail.com>
2025-11-01 18:39:02 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "apiKey": "AIzaSyBNyHPG_bsmOVfKP1T--iDaLN888EFwclo",
+  "apiKey": "AIzaSyAnbdNFScpA1lhbPj0U4VIDcSaqLAVpxi4",
   "authDomain": "nettecksolutions.firebaseapp.com",
   "projectId": "nettecksolutions",
-  "storageBucket": "nettecksolutions.appspot.com",
+  "storageBucket": "nettecksolutions.firebasestorage.app",
   "messagingSenderId": "876860454731",
   "appId": "1:876860454731:web:5d3963d04199931f789ace",
   "measurementId": "G-W3Z0NTXKLY"
