6a0736ce9fa4a7452ab53e42f6a9721cfa4db5d8
Fidele <nshutifidele1@gmail.com>
2025-11-01 12:24:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2025717843-fcd9d",
+  "appId": "1:990673220131:web:c378c60efef342a981f796",
+  "apiKey": "AIzaSyBNyHPG_bsmOVfKP1T--iDaLN888EFwclo",
+  "authDomain": "studio-2025717843-fcd9d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "990673220131"
+};
